7e922e029061744bdc87bddeb41e5d3e69bd4f1d
Thalyta Sousa <thalytac.desousa@hotmail.com>
2024-04-02 19:18:14 -0300

    projeto incial

---
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -0,0 +1,67 @@
+let listaDeNumerosSorteados = [];
+let numeroLimite = 10;
+let tentativas = 1;
+let numeroSecreto;
+
+
+numeroSecreto = gerarNumero();
+
+function gerarNumero(){
+    let numeroEscolhido = parseInt(Math.random() * numeroLimite + 1);
+    let  quantidadeDeElementosDaLista = listaDeNumerosSorteados.length;
+
+    if(quantidadeDeElementosDaLista == numeroLimite){
+        listaDeNumerosSorteados = [];
+    }
+   
+    if(listaDeNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumero();
+    }else{
+        listaDeNumerosSorteados.push(numeroEscolhido);
+        return numeroEscolhido;
+    }
+}
+function verificarChute(){
+    let chute = document.querySelector('input').value;
+    
+    if(chute == numeroSecreto){
+        exibirTexto('h1','Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas': 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa} `;
+        exibirTexto('p',mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    }else{
+        if (chute > numeroSecreto){
+            exibirTexto('p','O número é menor');
+        }else{
+            exibirTexto('p','O número é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+function mensagemInicial(){
+    exibirTexto('h1','Jogo do número Secreto');
+    exibirTexto('p','Escolha um número entre 1 e 10');
+}
+
+function novoJogo(){
+    numeroSecreto = gerarNumero();
+    limparCampo();
+    tentativas = 1;
+    mensagemInicial();
+}
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+function exibirTexto(tag,texto){
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+    responsiveVoice.speak(texto,'Brazilian Portuguese Female',{rate:1.2});
+}
+
+document.getElementById('reiniciar').addEventListener('click', novoJogo);
+mensagemInicial();
+
